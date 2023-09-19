17496fb20b58b6c65e23758a39c1576b23a29cd1
Traven Gonzales <travengonzales@gmail.com>
2023-09-19 14:38:07 -0700

    feat(register-endpoint): Added register endpoint for auth server

--- backend/authentication/server.js
+++ backend/authentication/server.js
@@ -1,13 +1,76 @@
 const express = require("express");
 const bcrypt = require("bcrypt");
-
+const cors = require("cors");
 const jwt = require("jsonwebtoken");
+const mysql = require("mysql2/promise");
+const uuid = require("uuid");
 
+// Server Configuration
 require("dotenv").config();
 
 const app = express();
 app.use(express.json());
 
-const PORT = process.env.AUTH_PORT || 5000;
+const corsOptions = {
+    origin: "*",
+    credentials: true,
+    optionSuccessStatus: 200
+};
 
-app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+const pool = mysql.createPool({
+    host: process.env.APP_DB_HOST,
+    user: process.env.APP_DB_USER,
+    password: process.env.APP_DB_PASSWORD,
+    database: process.env.APP_DB_DATABASE
+});
+
+// Constants
+const PORT = process.env.AUTH_PORT || 5000;
+const SALT = 10;
+
+// DB Pool Middleware Configuration
+app.use(async function(req, res, next) {
+    try {
+        req.db = await pool.getConnection();
+        req.db.connection.config.namedPlaceholders = true;
+
+        await req.db.query(`SET SESSION sql_mode = "TRADITIONAL"`);
+        await req.db.query(`SET time_zone = '-8:00'`);
+
+        await next();
+
+        req.db.release();
+    } catch (err) {
+        console.log(err);
+
+        if (req.db) req.db.release();
+        throw err;
+    }
+});
+
+// Routes
+app.post("/register", async (req, res) => {
+    try {
+        const hashedPassword = await bcrypt.hash(
+            req.body.password, SALT
+        );
+            
+        const user = { 
+            id: uuid.v4(),
+            username: req.body.username, 
+            password: hashedPassword
+        };
+    
+        await req.db.query(
+            `INSERT INTO User (id, username, password)
+             VALUES (:id, :username, :password)`,
+             user
+        );
+    
+        res.json({ meme: "meme" });
+    } catch (error) {
+        res.json({ status: 500, error: error });
+    }
+});
+
+app.listen(PORT, () => console.log(`Auth Server start on port ${PORT}`));
---
+++ backend/schema/Users.sql
@@ -0,0 +1,12 @@
+CREATE TABLE User (
+    id VARCHAR(36) PRIMARY KEY,
+    username VARCHAR(32) NOT NULL,
+    password VARCHAR(60) NOT NULL 
+);
+
+-- Debugging
+-- Delete tables
+DROP TABLE User;
+
+-- View Table
+SELECT * FROM User;
