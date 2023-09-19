eaba592014567d4afea5eb61273387740ef9d21e
Traven Gonzales <travengonzales@gmail.com>
2023-09-19 16:22:12 -0700

    refactor(user-route): Refactored authentication server as users route for server

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,52 +1,14 @@
 const express = require("express");
 const bcrypt = require("bcrypt");
-const cors = require("cors");
 const jwt = require("jsonwebtoken");
-const mysql = require("mysql2/promise");
 const uuid = require("uuid");
 
-// Server Configuration
+// Route Configuration
+const router = express.Router();
 require("dotenv").config();
 
-const app = express();
-app.use(express.json());
-
-const corsOptions = {
-    origin: "*",
-    credentials: true,
-    optionSuccessStatus: 200
-};
-
-const pool = mysql.createPool({
-    host: process.env.APP_DB_HOST,
-    user: process.env.APP_DB_USER,
-    password: process.env.APP_DB_PASSWORD,
-    database: process.env.APP_DB_DATABASE
-});
-
 // Constants
-const PORT = process.env.AUTH_PORT || 5000;
 const SALT = 10;
-
-// DB Pool Middleware Configuration
-app.use(async function(req, res, next) {
-    try {
-        req.db = await pool.getConnection();
-        req.db.connection.config.namedPlaceholders = true;
-
-        await req.db.query(`SET SESSION sql_mode = "TRADITIONAL"`);
-        await req.db.query(`SET time_zone = '-8:00'`);
-
-        await next();
-
-        req.db.release();
-    } catch (err) {
-        console.log(err);
-
-        if (req.db) req.db.release();
-        throw err;
-    }
-});
 
 // Authorization Helper Functions
 const generateAccessToken = (user) => {
@@ -81,7 +43,7 @@ const authenticateToken = (req, res, next) => {
 }
 
 // Routes
-app.post("/register", async (req, res) => {
+router.post("/register", async (req, res) => {
     try {
         const hashedPassword = await bcrypt.hash(
             req.body.password, SALT
@@ -105,7 +67,7 @@ app.post("/register", async (req, res) => {
     }
 });
 
-app.post("/login", async (req, res) => {
+router.post("/login", async (req, res) => {
     try {
         const { username, password } = req.body;
 
@@ -141,5 +103,4 @@ app.post("/login", async (req, res) => {
     }
 });
 
-// Run App
-app.listen(PORT, () => console.log(`Auth Server start on port ${PORT}`));
+module.exports = router;
---
+++ backend/server.js
@@ -0,0 +1,55 @@
+const express = require("express");
+const cors = require("cors");
+const mysql = require("mysql2/promise");
+
+const usersRouter = require("./routes/users.js");
+
+// Server Configuration
+require("dotenv").config();
+
+const app = express();
+app.use(express.json());
+
+const corsOptions = {
+    origin: "*",
+    credentials: true,
+    optionSuccessStatus: 200
+};
+app.use(cors(corsOptions));
+
+const pool = mysql.createPool({
+    host: process.env.APP_DB_HOST,
+    user: process.env.APP_DB_USER,
+    password: process.env.APP_DB_PASSWORD,
+    database: process.env.APP_DB_DATABASE
+});
+
+
+// Constants
+const PORT = process.env.AUTH_PORT || 5000;
+
+// Middleware Configuration
+app.use(async function(req, res, next) {
+    try {
+        req.db = await pool.getConnection();
+        req.db.connection.config.namedPlaceholders = true;
+
+        await req.db.query(`SET SESSION sql_mode = "TRADITIONAL"`);
+        await req.db.query(`SET time_zone = '-8:00'`);
+
+        await next();
+
+        req.db.release();
+    } catch (err) {
+        console.log(err);
+
+        if (req.db) req.db.release();
+        throw err;
+    }
+});
+
+app.use("/users", usersRouter);
+
+
+// Run App
+app.listen(PORT, () => console.log(`Server start on port ${PORT}`));
