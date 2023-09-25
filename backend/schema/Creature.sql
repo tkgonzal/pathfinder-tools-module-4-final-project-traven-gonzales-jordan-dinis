b17b84a96ae76b2b93f603470dbae2dd9b37e1a6
Traven Gonzales <travengonzales@gmail.com>
2023-09-25 14:59:41 -0700

    feat(encounter-schema): Made schema for Encounter and Creature tables for backend

---
+++ backend/schema/Creature.sql
@@ -0,0 +1,15 @@
+CREATE TABLE Creature (
+    id VARCHAR(36) PRIMARY KEY,
+    encounter_id VARCHAR(36) NOT NULL,
+    level TINYINT NOT NULL,
+    xp INT NOT NULL,
+    quantity INT NOT NULL,
+    FOREIGN KEY (encounter_id) REFERENCES Encounter(id)
+);
+
+-- Debugging
+-- Delete tables
+DROP TABLE Creature;
+
+-- View Table
+SELECT * FROM Creature;
---
+++ backend/schema/Encounter.sql
@@ -0,0 +1,16 @@
+CREATE TABLE Encounter (
+    id VARCHAR(36) PRIMARY KEY,
+    user_id VARCHAR(36) NOT NULL,
+    name VARCHAR(100) NOT NULL,
+    party_level TINYINT NOT NULL,
+    party_size TINYINT NOT NULL,
+    threat_level VARCHAR(8) NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES User(id)
+);
+
+-- Debugging
+-- Delete tables
+DROP TABLE Encounter;
+
+-- View Table
+SELECT * FROM Encounter;
